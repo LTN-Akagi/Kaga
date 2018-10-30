4236d1412d23a434e89a3e7bb54930a9f55c936e
Unknown <haydenbonham123@gmail.com>
2018-10-30 18:09:06 -0500

    Had to wait on Techna

--- src/main/java/com/avairebot/shared/DiscordConstants.java
+++ src/main/java/com/avairebot/shared/DiscordConstants.java
@@ -8,5 +8,5 @@ public class DiscordConstants {
     public static final long SENITHER_BOT_ID = 111111111111111111L;
     public static final long FEEDBACK_CHANNEL_ID = 506960689546526750L;
     public static final long ACTIVITY_LOG_CHANNEL_ID = 506953497565069312L;
-    public static final long BOT_ADMIN_EXCEPTION_ROLE = 11111111111111111L;
+    public static final long BOT_ADMIN_EXCEPTION_ROLE = 482010839055073280L;
 }
