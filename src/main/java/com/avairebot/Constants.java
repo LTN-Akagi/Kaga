7b3ced513b86ac2b175925236fff841e6ce72de6
Unknown <haydenbonham123@gmail.com>
2018-12-31 19:07:42 -0800

    Triple Boop

--- src/main/java/com/avairebot/Constants.java
+++ src/main/java/com/avairebot/Constants.java
@@ -48,7 +48,7 @@ public class Constants {
     public static final String PACKAGE_JOB_PATH = "com.avairebot.scheduler";
 
     // Emojis
-    public static final String EMOTE_ONLINE = "<:online:519340268839895061>";
+    public static final String EMOTE_ONLINE = "<:online:519338699788648458>";
     public static final String EMOTE_AWAY = "<:away:519335317262696449>";
     public static final String EMOTE_DND = "<:dnd:519335317262696449>";
 
